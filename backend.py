d9c1bf65ea6ca138ceefd3e2fc029fc0978d2aa5
SilentStorm3 <120201535+SilentStorm3@users.noreply.github.com>
2024-10-19 22:07:03 +0530

    Upload

---
+++ backend.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, jsonify
+import pymongo
+
+app = Flask(__name__)
+
+# Connect to MongoDB
+client = pymongo.MongoClient("mongodb+srv://amourphouskitten:funnypassword@navigationapp.zdicc.mongodb.net/?retryWrites=true&w=majority&appName=NavigationApp")
+db = client["Rooms"]
+
+collections = {
+    "students": db["Students"],
+    "professors": db["Profs"],
+    "laboratories": db["Labs"]
+}
+
+# Function to fetch a response from the appropriate collection based on user input and category
+def fetch_response(user_input, category):
+    collection = collections[category]
+    result = collection.find_one({"Name": user_input})
+    return result
+
+
+# Function to find the closest matches if exact match is not found
+def find_closest_matches(user_input, category):
+    collection = collections[category]
+    matches = collection.find({"$text": {"$search": user_input}}).limit(5)
+    return [match['Name'] for match in matches]
+
+
+@app.route('/process', methods=['POST'])
+def process_input():
+    data = request.get_json()
+    user_input = data['input']
+    category = data['category']
+
+    # Fetch response from the appropriate collection
+    result = fetch_response(user_input, category)
+
+    if result:
+        response = {
+            'Name' : result['Name'],
+            'output': result['RoomNo'],
+            'lat': result['Lat'],
+            'long': result['Long'],
+            'suggestions': ''
+        }
+    else:
+        closest_matches = find_closest_matches(user_input, category)
+        response = {
+            'Name' : '',
+            'output': 'Did you mean one of these?',
+            'suggestions': closest_matches
+        }
+
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
